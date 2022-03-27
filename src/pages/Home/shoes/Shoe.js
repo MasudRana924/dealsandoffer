5fdb3cd3487c9a4734d40adac96c717dda45668c
MasudRana924 <masud15-924@diu.edu.bd>
2022-03-27 12:55:49 +0600

    cart changed

---
+++ src/pages/Home/shoes/Shoe.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+const Shoe = (props) => {
+    const { name, price, img, stock, url, brand } = props.shoe
+    return (
+        <Col className="mt-3">
+            <div className="card-shoes">
+                <p className="stock">{stock}</p>
+                <img src={img} className="w-75 mx-auto card-image" alt="" />
+                <h5 className="card-title">{name}</h5>
+                <p>{price}TK</p>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                    <button className="btn-cart">Purchase</button>
+                </a>
+
+            </div>
+
+        </Col>
+    );
+};
+
+export default Shoe;
